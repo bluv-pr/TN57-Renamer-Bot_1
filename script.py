4619c7e2340dd2f8c66457af90f18ad4e88d284c
Balaji Shinnosuke <62749324+bluv-pr@users.noreply.github.com>
2021-04-23 21:58:40 +0530

    Add files via upload

---
+++ script.py
@@ -0,0 +1,21 @@
+class script(object):
+    START_TEXT = """ A Exclusive File Renamer Bot With Permanent Thumbnail support!💯. \nBy  @TN57_BotZ 🤓
+
+<b>Send me any Telegram file and choose appropriate option! </b>"""
+
+    RENAME_403_ERR = "What Are You Doing? You are Banned"
+    UPGRADE_TEXT = "CONTACT @BluVDs"
+    DOWNLOAD_START = "Downloading Your File To My Server...😼"
+    UPLOAD_START = "Starting to upload...😇"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using Me > Join Here For More Bots @TN57_BotZ 📡 **"
+    SAVED_THUMB = "Thumbnail Saved ✅ This Is Permanent"
+    DEL_THUMB = "Thumbnail cleared succesfully!"
+    NO_THUMB = "No thumbnails found!"
+    SAVED_RECVD_DOC_FILE = "File Downloaded Successfully 😎"
+    CUSTOM_CAPTION_UL_FILE = " "
+    HELP_USER = """It's not that complicated😅
+    
+1. Send me any Telegram File.
+2. Choose appropriate option.
+#. BY : @TN57_BotZ."""
+
